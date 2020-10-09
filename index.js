1e3fb0783d574224ba5b44cde69ae87cdcf1ca68
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-09 15:26:11 -0700

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,50 @@
  for you to use if you need it!
  */
 
+function createEmployeeRecord(array) {
+  let obj = {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+  return obj;
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+  return arrayOfArrays.map(employee => createEmployeeRecord(employee));
+}
+
+function createTimeInEvent(dateStamp) {
+  this.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(dateStamp.split(" ")[1]),
+    date: dateStamp.split(" ")[0]
+  })
+  return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(dateStamp.split(" ")[1]),
+    date: dateStamp.split(" ")[0]
+  })
+  return this;
+}
+
+function hoursWorkedOnDate(date) {
+  let timeOut = this.timeOutEvents.find(event => event.date === date);
+  let timeIn = this.timeInEvents.find(event => event.date === date);
+  return (timeOut.hour - timeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+  return this.payPerHour * hoursWorkedOnDate.call(this, date);
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +63,14 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(records, firstName) {
+ return records.find(record => record.firstName === firstName);
+}
+
+function calculatePayroll(records) {
+  return records.reduce(function(total, record) {
+    return total + allWagesFor.call(record)
+  }, 0)
 }

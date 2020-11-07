656b34a6278884fd4a8ae66dee304f2c51dbcee3
felipe <felipebeltran.nyc@gmail.com>
2020-11-06 21:00:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,60 @@
 /* Your Code Here */
 
+let createEmployeeRecord = function(array){
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(array){
+    return array.map(function(row){
+        return createEmployeeRecord(row)
+    })
+}
+
+let createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(" ")
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+    })
+    return this
+} 
+let createTimeOutEvent = function(dataStamp){
+    let [date,hour] = dataStamp.split(" ")
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(date){
+    let inEvent = this.timeInEvents.find(function(e){
+        return e.date === date
+    })
+    let outEvent = this.timeOutEvents.find(function(e){
+        return e.date === date
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+ let wagesEarnedOnDate = function(date){
+     let wage = hoursWorkedOnDate.call(this, date) * this.payPerHour
+     return parseFloat(wage.toString()) 
+     }
+ 
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -19,4 +74,16 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+let findEmployeeByFirstName = function(srcArray, firstName){
+    return srcArray.find(function(record){
+        return record.firstName === firstName
+    })
+}
+
+let calculatePayroll = function(employees){
+    return employees.reduce(function(memo, record){
+        return memo + allWagesFor.call(record)
+    }, 0 )
 }
